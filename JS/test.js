121011712e25ece2d3f6fc20c1eec78afe1cf7cb
KsosAkuma <80045865+KsosAkuma@users.noreply.github.com>
2021-03-04 16:44:37 +0100

    Add files via upload

---
+++ JS/test.js
@@ -0,0 +1,12 @@
+var currentPage = 0;
+var orc = document.createElement(img);
+img.className=("orc");
+if (currentPage = 0){
+    
+}else if (currentPage = 1) {
+    
+}else if (currentPage = 2) {
+    
+}else if (currentPage = 3) {
+    
+};
